13bc5c5b6cce1c433f88ba6a39e771509c15944a
Andrey Mukamolow <fobo66@protonmail.com>
2017-11-22 10:52:25 +0300

    Added tact counting (macOS)

--- main.cpp
+++ main.cpp
@@ -1,5 +1,11 @@
 // MMX example
 #include <x86intrin.h>
+#include <mach/mach.h>
+#include <mach/mach_time.h>
+#include <unistd.h>
+
+#include <iostream>
+
 #include<opencv2/imgproc/imgproc.hpp>
 #include<opencv2/highgui/highgui.hpp>
 
@@ -59,19 +65,25 @@ int main(int argc, char* argv[]) {
 	char const *proc_file = "lena_proc.bmp";
 	char const *proc2_file = "lena_proc2.bmp";
 
+    uint64_t before = 0;
+    uint64_t after = 0;
+    uint64_t asmTimer = 0;
+    uint64_t nativeTimer = 0;
+
     Mat src, dst, asmDst;
+    Mat grad_x, grad_y;
+    Mat abs_grad_x, abs_grad_y;
+
     int gx, gy, sum;
  
       // Load an image
     src = imread(src_file, CV_LOAD_IMAGE_GRAYSCALE);
-    GaussianBlur( src, src, Size(3,3), 0, 0, BORDER_DEFAULT );
      
     if( !src.data )
     { return -1;  }
  
- 
-    Mat grad_x, grad_y;
-    Mat abs_grad_x, abs_grad_y;
+    before = mach_absolute_time();
+    GaussianBlur( src, src, Size(3,3), 0, 0, BORDER_DEFAULT );
 
     // Gradient X
     Sobel(src, grad_x, CV_16S, 1, 0);
@@ -81,8 +93,12 @@ int main(int argc, char* argv[]) {
     convertScaleAbs( grad_x, abs_grad_x );
     convertScaleAbs( grad_y, abs_grad_y );
     addWeighted( abs_grad_x, 0.5, abs_grad_y, 0.5, 0, dst);
+    after = mach_absolute_time();
+    nativeTimer = after - before;
+
 
     asmDst = src.clone();
+    before = mach_absolute_time();
     for (int y = 1; y < src.rows - 1; y++) {
         for (int x = 1; x < src.cols - 1; x++) {
             gx = xGradientAsm(src, x, y);
@@ -93,7 +109,8 @@ int main(int argc, char* argv[]) {
             asmDst.at<uchar>(y,x) = sum;
         }
     }
-
+    after = mach_absolute_time();
+    asmTimer = after - before;
  
     namedWindow("C++ Sobel");
     imshow("C++ Sobel", dst);
@@ -106,7 +123,9 @@ int main(int argc, char* argv[]) {
  
     namedWindow("initial");
     imshow("initial", src);
- 
+    
+    std::cout << "C++ timing: " << nativeTimer << " Assembly timing: " << asmTimer << std::endl;
+
     waitKey(0);
 
     return 0;
